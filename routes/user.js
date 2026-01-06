1ba1db3bf31e7e0353465b9269adcd97148490c1
Ayinmiro tobi <ayinmirotobi@gmail.com>
2026-01-06 13:48:20 +0100

    Delete routes directory

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRouter = express.Router()
-const UserController = require('../controller/userController')
-
-userRouter.post('/login', UserController.loginController)
-userRouter.post('/signin', UserController.signinController)
-
-module.exports = userRouter
