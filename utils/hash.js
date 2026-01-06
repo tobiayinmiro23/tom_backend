6ad7b84b61c1eb8416735206bb0cd3b3754d7bee
Ayinmiro tobi <ayinmirotobi@gmail.com>
2026-01-06 13:47:33 +0100

    Delete utils directory

--- utils/hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-const hashPassword = (password) => {
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(password, salt);
-    return hash
-
-}
-module.exports = hashPassword
